de982e9b8618300c961abf4528d77c2a2d41d65e
kavirash123 <rkaveesha77@gmail.com>
2024-12-01 00:07:56 +0000

    Add files via upload

---
+++ App.js
@@ -0,0 +1,20 @@
+import React from "react";
+import KeyPadComponent from "./Calculator";
+import "./App.css";
+
+function App() {
+    return (
+        <div>
+            <h1>React Calculator</h1>
+            <KeyPadComponent />
+        </div>
+    );
+}
+
+export default App;
+
+
+
+
+
+
